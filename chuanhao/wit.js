5d77e40941073b0a06d187903d6c1998a64e46e8
chuanhao01 <chuanhaolim@gmail.com>
2020-06-09 21:27:51 +0800

    init wit module

---
+++ chuanhao/wit.js
@@ -0,0 +1,11 @@
+/**
+ * @fileoverview
+ * This file mainly serves as a module to make request to the wit ai api and return the request so as to make interfacing easier
+ * Mainly require this file to have access to the main module wit
+ * 
+ */
+
+const wit = {
+
+};
+
